1107992aa4fc4fad9dbfed2fe5f6b83046ecc26b
Quico Saval <quicosv@gmail.com>
2023-06-26 20:02:38 +0200

    Añade las rutas de los clientes

--- routes/routerClientes.ts
+++ routes/routerClientes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getClientes } from "../controllers/clientesController";
+import { deleteCliente, getClientePorId, getClientes, insertCliente, putCliente } from "../controllers/clientesController";
 
 export const routerClientes = Router();
 routerClientes.get('/', getClientes);
+routerClientes.get('/:id', getClientePorId);
+routerClientes.post('/', insertCliente);
+routerClientes.put('/:id', putCliente);
+routerClientes.delete('/:id', deleteCliente);
