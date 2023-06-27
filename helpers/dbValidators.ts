29d83ddcc6c55d23ce56959c7e2f9aea2363f727
Quico Saval <quicosv@gmail.com>
2023-06-27 18:59:21 +0200

    Termina los checks de las rutas post y put de clientes

--- helpers/dbValidators.ts
+++ helpers/dbValidators.ts
@@ -1,6 +1,6 @@
 import { Cliente } from "../models/clientes";
 
-const existeClientePorId = async (id: number) => {
+export const existeClientePorId = async (id: number) => {
 	const cliente = await Cliente.findByPk(id);
 if(!cliente){
 	throw new Error('No existe ningún cliente con este id.')
--- routes/routerClientes.ts
+++ routes/routerClientes.ts
@@ -1,9 +1,37 @@
 import { Router } from "express";
 import { deleteCliente, getClientePorId, getClientes, insertCliente, putCliente } from "../controllers/clientesController";
+import { check, param } from "express-validator";
+import { existeClientePorId } from "../helpers/dbValidators";
+import { validarCampos } from "../middlewares/validarCampos";
 
 export const routerClientes = Router();
 routerClientes.get('/', getClientes);
-routerClientes.get('/:id', getClientePorId);
-routerClientes.post('/', insertCliente);
-routerClientes.put('/:id', putCliente);
-routerClientes.delete('/:id', deleteCliente);
+routerClientes.get('/:id',
+	[param('id').exists().isNumeric().custom(existeClientePorId), validarCampos],
+	getClientePorId);
+routerClientes.post('/',
+	[
+		check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+		check('poblacion', 'La población es obligatoria').not().isEmpty(),
+		check('telefono', 'El teléfono es obligatorio').not().isEmpty().isLength({
+			min: 9,
+			max: 9
+		}),
+		validarCampos
+	],
+	insertCliente);
+routerClientes.put('/:id',
+	[
+		param('id').exists().isNumeric().custom(existeClientePorId),
+		check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+		check('poblacion', 'La población es obligatoria').not().isEmpty(),
+		check('telefono', 'El teléfono es obligatorio').not().isEmpty().isLength({
+			max: 9,
+			min: 9
+		}),
+		validarCampos
+	],
+	putCliente);
+routerClientes.delete('/:id',
+	[param('id').exists().isNumeric().custom(existeClientePorId), validarCampos],
+	deleteCliente);
